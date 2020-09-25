5521a8937cb28a7ce6f4ef20b00ee5be9a976e41
ummikolbia22 <miikolbia@gmail.com>
2020-09-25 21:49:02 +0700

    Upload Project

---
+++ app/src/main/java/com/ummi/myprofile/BiodataProfile.kt
@@ -0,0 +1,11 @@
+package com.ummi.myprofile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BiodataProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_biodata_profile)
+    }
+}
